f108867b3bbec8b1930c68ac0ab9200be72aeca0
Burak SALCAN <buraksalcan@gmail.com>
2024-12-30 00:59:15 +0300

    29.12.2024

---
+++ AttributeKavrami/AttributeKavrami/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeKavrami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tanımlama
+
+            //.net uygulamalarıtemel olarak, program kodu, veri ve meta datadan oluşur. Metadata, veri hakkında veri olarak tanımlanır ve programın binary dosyalarının(exe veya dll) içine gömülür. 
+            //Attributelar(Nitelikler) ile programımıza ve onun elemanlarına derleyici direktifleri(yönetmen)(compiler Instractions) ve diğer metadata bilgilerini ekleyebiliriz. 
+            //Attribute'lar:
+            //- Sınıflara
+            //- Sınıf Üyelerine(field, functions, constructor)
+            //- Arayüzlere
+            //- Enumlara
+            //Eklenebilirler. 
+
+            #endregion
+
+            #region Kullanım
+
+            Test test = new Test();
+            test.Metot_A();
+            test.Metot_B();
+            test.Metot_C();
+
+            //Console.ReadKey();
+
+            #endregion
+        }
+    }
+
+    class Test
+    {
+        public void Metot_A()
+        {
+            Console.WriteLine("Metot A'dan Merhaba");
+        }
+        [Conditional("DEBUG")]//Üstüne konduğu şeyi debug modda çalıştırır.
+        public void Metot_B()
+        {
+            Console.WriteLine("Metot b'den Merhaba");
+        }
+        public void Metot_C()
+        {
+            Console.WriteLine("Metot C'den Merhaba");
+        }
+    }
+}
